796d979dd907305c6a83decbcde4fa3b629df795
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2015-12-22 00:35:52 -0500

    SS: Update brew and brew cask

--- zsh_aliases.sh
+++ zsh_aliases.sh
@@ -1,86 +1,88 @@
 # Aliases for zsh
-alias c="clear"
-alias md="mkdir -pv"
-alias h="history"
-alias mv='mv -i'
-alias cp='cp -irp'
-alias ln='ln -i'
+alias -g c="clear"
+alias -g md="mkdir -pv"
+alias -g h="history"
+alias -g mv='mv -i'
+alias -g cp='cp -irp'
+alias -g ln='ln -i'
 
 # resume wget by default #
-alias wget='wget -c'
+alias -g wget='wget -c'
 
 #copy output of last command to clipboard
-alias clcmd="fc -e -| pbcopy"
+alias -g clcmd="fc -e -| pbcopy"
 
 # copy the working directory path
-alias cpwd='pwd | tr -d "\n" | pbcopy'
+alias -g cpwd='pwd | tr -d "\n" | pbcopy'
 
 # ssh
-alias ssh="ssh -X"
+alias -g ssh="ssh -X"
 
 # Common shell functions
-alias less='less -r'
-alias tf='tail -f'
-alias screen='TERM=screen screen'
-alias cls='clear'
+alias -g less='less -r'
+alias -g tf='tail -f'
+alias -g screen='TERM=screen screen'
+alias -g cls='clear'
 
 # Finder
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias -g showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias -g hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+alias -g brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+alias -g brewcasku='brew update && brew cleanup && brew cask cleanup && brew cask update'
 
 # Show human friendly numbers and colors
-alias df='df -h'
-alias du='du -h -d 2'
+alias -g df='df -h'
+alias -g du='du -h -d 2'
 
 # mimic vim functions
-alias :q='exit'
+alias -g :q='exit'
 
 # Application aliases
-alias chrome='open $HOME/Applications/"Google Chrome.app"'
-alias skype='open $HOME/Applications/Skype.app'
-alias gdrive='open $HOME/Applications/"Google Drive.app"'
-alias slack='open $HOME/Applications/Slack.app'
-alias jdownloader='open $HOME/Applications/JDownloader2.app'
-alias vlc='open $HOME/Applications/VLC.app'
-alias djvu='open $HOME/Applications/DjView.app'
-alias rstudio="open -a Rstudio" #Rstudio
+alias -g chrome='open $HOME/Applications/"Google Chrome.app"'
+alias -g skype='open $HOME/Applications/Skype.app'
+alias -g gdrive='open $HOME/Applications/"Google Drive.app"'
+alias -g slack='open $HOME/Applications/Slack.app'
+alias -g jdownloader='open $HOME/Applications/JDownloader2.app'
+alias -g vlc='open $HOME/Applications/VLC.app'
+alias -g djvu='open $HOME/Applications/DjView.app'
+alias -g rstudio="open -a Rstudio" #Rstudio
 
 # ls better
-alias la="ls -al"
-alias ld="ls -ld"
-alias ll="ls -alF"
+alias -g la="ls -al"
+alias -g ld="ls -ld"
+alias -g ll="ls -alF"
 
-# alias hub as git
-# alias git="hub"
+# alias -g hub as git
+# alias -g git="hub"
 
 # Git Aliases
-alias ga="git add"
-alias gaa='git add -A'
-alias gb="git branch"
-alias gbd="git branch -d"
-alias gc="git commit -m"
-alias gcb="git checkout -b"
-alias gcp='git cp'
-alias gcm="git checkout master"
-alias gcln='git clean'
-alias gd='git diff'
-alias gdc='git diff --cached -w'
-alias gds='git diff --staged -w'
-alias gf='git fetch'
-alias gi='atom .gitignore'
-alias gl="git log --pretty=oneline --graph --all --topo-order"
-alias gm='git merge'
-alias grad='git remote add'
-alias gpl='git pull'
-alias gps='git push'
-alias grs='git reset'
-alias grv='git remote -v'
-alias gs='git status'
-alias gst='git stash'
-alias gsp='git stash pop'
-alias gshw='git show'
-alias guns='git unstage'
-alias gunc='git uncommit'
+alias -g ga="git add"
+alias -g gaa='git add -A'
+alias -g gb="git branch"
+alias -g gbd="git branch -d"
+alias -g gc="git commit -m"
+alias -g gcb="git checkout -b"
+alias -g gcp='git cp'
+alias -g gcm="git checkout master"
+alias -g gcln='git clean'
+alias -g gd='git diff'
+alias -g gdc='git diff --cached -w'
+alias -g gds='git diff --staged -w'
+alias -g gf='git fetch'
+alias -g gi='atom .gitignore'
+alias -g gl="git log --pretty=oneline --graph --all --topo-order"
+alias -g gm='git merge'
+alias -g gmv='git move'
+alias -g grad='git remote add'
+alias -g gpl='git pull'
+alias -g gps='git push'
+alias -g grs='git reset'
+alias -g grv='git remote -v'
+alias -g gs='git status'
+alias -g gst='git stash'
+alias -g gsp='git stash pop'
+alias -g gshw='git show'
+alias -g guns='git unstage'
+alias -g gunc='git uncommit'
